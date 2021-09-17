16b7262418665f20b841b0224f617ff2674b1e0b
Ali Roemhildt <aliroemhildt@gmail.com>
2021-09-16 20:20:42 -0600

    Add arrays

--- model-1.js
+++ model-1.js
@@ -1,6 +1,12 @@
+#integers
 var numOne = 2
 var numTwo = 7.5
 var numThree = 100678
 
+#booleans
 var isHappy = true
 var isStudent = false
+
+#arrays
+var ages = [25,16,12,45]
+var names = ['Ali','Daniel','Katie','Kevin']
